60e76eac92a86c5373e894e6936adaa74a13f168
L35Paul <joe.orndorff@idg.jhu.edu>
2018-11-26 14:12:15 -0500

    added db file and batch file

---
+++ RunPyCui5.bat
@@ -0,0 +1,4 @@
+P:
+cd P:/Sensor Monitor Board/Software/RaspberryPi/ICS_SMB/venv/Scripts
+pyuic5.exe "P:/Sensor Monitor Board/Software/RaspberryPi/ICS_SMB/SmbMainGui.ui" ^
+-o "P:/Sensor Monitor Board/Software/RaspberryPi/ICS_SMB/SmbMainGui.py"
