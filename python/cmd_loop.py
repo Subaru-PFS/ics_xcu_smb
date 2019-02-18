37ae5f1afc864c88f82f7d3b66851b7531345163
Craig Loomis <cloomis@astro.princeton.edu>
2019-02-18 07:10:06 -0500

    log command args

--- python/cmd_loop.py
+++ python/cmd_loop.py
@@ -109,7 +109,7 @@ class CmdLoop(threading.Thread):
             self.logger.warn('qxmit full!')
             return -1
 
-        self.logger.info('executing read cmd %s', cmd)
+        self.logger.info('executing read cmd %s %s', cmd, p1)
         
         # fetch Heater Mode (0=Disabled, 1=Fixed Percent, 2=PID Control)
         if cmd == 'L':
@@ -248,7 +248,7 @@ class CmdLoop(threading.Thread):
         p2max = cmd_dict["P2_MAX"]
         output =  '~' + cmd + ', ' + str(p1) +  ', ' + str(p2)
 
-        self.logger.info('executing write cmd %s', cmd)
+        self.logger.info('executing write cmd %s %s %s', cmd, p1, p2)
 
         if p1 < p1min or p1 > p1max or p2 < p2min or p2 > p2max:
             output = "bad command"
